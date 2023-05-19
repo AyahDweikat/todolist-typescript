70c2806305d4c1a37c3d7d2bba3d910c46f41f50
AyahDweikat <dweikatayah99@gmail.com>
2023-05-19 21:28:33 +0300

    edits on app component, adding utils

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import 'vite/modulepreload-polyfill'
+import { Dispatch, SetStateAction, useState } from "react";
+import "./App.css";
+import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
+import Header from './Components/Header/Header';
+import './responsive.css';
+import Main from './Components/Main/Main';
+
+
+function App() {
+  const [theme, setTheme]:[boolean, Dispatch<SetStateAction<boolean>>] = useState(false);
+  const [searchValue, setSearchValue]:[string, Dispatch<SetStateAction<string>>] = useState("");
+
+  
+  function handleChangeTheme():void {
+    setTheme(!theme);
+  }
+  return (
+    <div className={theme ? "dark-theme" : ""}>
+      <Header onChangeTheme={handleChangeTheme} theme={theme} searchValue={searchValue} onChangeSearchValue={setSearchValue} />
+      <Main searchValue={searchValue} />
+    </div>
+  );
+}
+
+export default App;
