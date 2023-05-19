37e6c8f3be6fefd01ad842cc0a1edb5907f21b90
AyahDweikat <dweikatayah99@gmail.com>
2023-05-19 21:32:07 +0300

    modal for delete tasks

---
+++ src/Components/Tasks/ModaLTask/ModalTask.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Task } from "../../../utilsInterface";
+import styles from "./modalTasks.module.css";
+
+type PropsModalTask = {
+  handleClose: () => void;
+  deleteTask: (id: string, allTasks: Task[]) => void;
+  id: string;
+  tasks: Task[];
+};
+const ModalTask: React.FC<PropsModalTask> = ({
+  handleClose,
+  deleteTask,
+  id,
+  tasks,
+}) => {
+  return (
+    <div className={styles.modal}>
+      <h3>Delete Task</h3>
+      <p>Are you sure you want to delete this task?</p>
+      <span>
+        This is a permenant deletion, you won&apos;t be able to undo it
+      </span>
+      <div className={styles.modalBtns}>
+        <button
+          type="button"
+          data-bs-dismiss="modal"
+          onClick={() => handleClose()}
+        >
+          Cancel
+        </button>
+        <button
+          type="button"
+          id="delBtn"
+          onClick={() => {
+            deleteTask(id, tasks);
+            handleClose();
+          }}
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ModalTask;
